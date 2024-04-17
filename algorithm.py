f802afbd0290e075c203c28bca99a26cfd8359fc
yutelkova <yutelkova@phoenixit.ru>
2024-04-17 09:50:12 +0300

    add algorithm for graph and lenght

--- algorithm.py
+++ algorithm.py
@@ -1,17 +1,40 @@
-def dfs(graph, start, visited=None):
-    if visited is None:
-        visited = set()
-    visited.add(start)
-    print(start)
-    for next in graph[start] - visited:
-        dfs(graph, next, visited)
-    return visited
+import sys
 
 
-graph = {'0': set(['1', '2']),
-         '1': set(['0', '3', '4']),
-         '2': set(['0']),
-         '3': set(['1']),
-         '4': set(['2', '3'])}
+class Graph(object):
+    def __init__(self, nodes, init_graph):
+        self.nodes = nodes
+        self.graph = self.construct_graph(nodes, init_graph)
 
-dfs(graph, '0')
+    def construct_graph(self, nodes, init_graph):
+        '''
+        Этот метод обеспечивает симметричность графика. Другими словами, если существует путь от узла A к B со значением V, должен быть путь от узла B к узлу A со значением V.
+        '''
+        graph = {}
+        for node in nodes:
+            graph[node] = {}
+
+        graph.update(init_graph)
+
+        for node, edges in graph.items():
+            for adjacent_node, value in edges.items():
+                if graph[adjacent_node].get(node, False) == False:
+                    graph[adjacent_node][node] = value
+
+        return graph
+
+    def get_nodes(self):
+        "Возвращает узлы графа"
+        return self.nodes
+
+    def get_outgoing_edges(self, node):
+        "Возвращает соседей узла"
+        connections = []
+        for out_node in self.nodes:
+            if self.graph[node].get(out_node, False) != False:
+                connections.append(out_node)
+        return connections
+
+    def value(self, node1, node2):
+        "Возвращает значение ребра между двумя узлами."
+        return self.graph[node1][node2]
